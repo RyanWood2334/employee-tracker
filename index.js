95a5aef33401c5e4ac7d9ccbb2d22f85007505a9
RyanWood2334 <ryanwooduwcoding.2334@gmail.com>
2023-05-01 20:07:25 -0700

    added file structure and pseudo-code

---
+++ index.js
@@ -0,0 +1,47 @@
+//build inquirer questions:
+//  -view all departments
+//  -view all employees
+//  -view all roles
+//  -add a department
+//  -add a role
+//  -add an employee
+//  -update an employee role
+//  -quit
+
+//add recursive functions for inquirer
+//view departments function
+//  name/id's
+
+//view role function
+//  -job title
+//  -role id
+//  -dept role belongs to
+//  -salary
+
+//view all employees function
+//  employee id's
+//  employee first names
+//  employee last names
+//  employee job titles
+//  employee departments
+//  employee salaries
+//  employee managers that they report to
+
+//add a department
+//  -name of dept
+
+//add a role
+//  -name
+//  -salary
+//  -department for role
+
+//add an employee
+//  -first name
+//  -last name
+//  -role
+//  -manager
+
+//update an employee role
+//  -new role
+//
+//
