9f9e91c4d73ae16f8f41ea80adc2d730d7083667
RyanWood2334 <ryanwooduwcoding.2334@gmail.com>
2023-05-01 22:02:41 -0700

    commented out the role and manager-id for now, but seeding works otherwise

--- db/seeds.sql
+++ db/seeds.sql
@@ -2,20 +2,9 @@ INSERT INTO departments (name)
 VALUES ("Ski Patrol"), ("Lift Operations"), ("Food And Beverage"), ("Retail");
 
 INSERT INTO
-    roles (
-        id,
-        title,
-        salary,
-        department_id
-    )
+    roles (title, salary)
 VALUES ("Patroller", 30000), ("Lift Operator", 20000), ("Cook", 22000), ("Cashier", 18000);
 
 INSERT INTO
-    employees (
-        id,
-        first_name,
-        last_name,
-        role_id,
-        manager_id
-    )
+    employees (first_name, last_name)
 VALUES ("Lebron", "James"), ("Dirk", "Nowitski"), ("Zion", "Williamson"), ("Geno", "Smith"), ("Christian", "McCaffrey"), ("Austin", "Ekeler");
